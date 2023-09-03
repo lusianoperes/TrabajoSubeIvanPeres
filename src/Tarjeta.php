b41240a9765d492fe213cb7ca9b62d62439de2bf
lusianoperes <luchoperez004@gmail.com>
2023-09-02 23:09:46 -0300

    añadida branch de descuento de saldos

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -5,6 +5,7 @@ namespace TrabajoSube;
 class Tarjeta{
 
     public const LIMITESALDO = 6600;
+    public const LIMITESALDONEGATIVO = -21184;
     public const VALORESDECARGAPERMITIDOS = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
     public $saldo;
 
